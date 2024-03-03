47911508c77553db94561a3d3ee387e828de9b90
Julie Spackman <spack84@byui.edu>
2024-03-02 20:07:05 -0700

    Wk9 Tutorials Begun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'http-02-post-requests-practice';
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http.post<{name: string}>(
+        // 'https://ng-complete-guide-c56d3.firebaseio.com/posts.json', //his original link in the starter code
+        'https://ng-complete-guide-e26ee-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-e26ee-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData=> {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+        // console.log(posts);
+      });
+  }
 }
